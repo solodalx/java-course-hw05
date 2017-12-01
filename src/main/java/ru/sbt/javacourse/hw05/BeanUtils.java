8c0b236979200f98d6295f31ee3123a44d913d0c
Alexey <a@a.ru>
2017-12-02 01:12:24 +0300

    Commit 02

--- src/main/java/ru/sbt/javacourse/hw05/BeanUtils.java
+++ src/main/java/ru/sbt/javacourse/hw05/BeanUtils.java
@@ -1,7 +1,12 @@
 package ru.sbt.javacourse.hw05;
 
+import java.lang.reflect.Method;
+
 public class BeanUtils {
     public static void assign(Object to, Object from) {
+
+        Method[] methodsFrom = from.getClass().getMethods();
+        Method[] methodsTo = to.getClass().getMethods();
 
     }
 }
