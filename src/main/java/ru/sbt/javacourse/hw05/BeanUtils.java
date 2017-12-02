ee90b4de1eb395f1f7c19587a52c7e7b3c7b009f
Alexey <a@a.ru>
2017-12-02 19:13:30 +0300

    Commit 04

--- src/main/java/ru/sbt/javacourse/hw05/BeanUtils.java
+++ src/main/java/ru/sbt/javacourse/hw05/BeanUtils.java
@@ -1,6 +1,8 @@
 package ru.sbt.javacourse.hw05;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BeanUtils {
     public static void assign(Object to, Object from) {
@@ -8,8 +10,25 @@ public class BeanUtils {
         Method[] methodsFrom = from.getClass().getMethods();
         Method[] methodsTo = to.getClass().getMethods();
 
-        for (Method method : methodsFrom) {
+        Map<String, Method> getters = new HashMap<String, Method>();
+        String name;
 
+        for (Method method : methodsFrom) {
+            name = method.getName();
+            if (name.startsWith("get") &&
+                    method.getParameterCount() == 0 &&
+                    method.getReturnType() != void.class &&
+                    method.getReturnType() != Void.class) {
+                getters.put(name.substring(3, name.length()), method);
+            }
+        }
+
+        for (Method method : methodsTo) {
+            name = method.getName();
+            if (name.startsWith("set") &&
+                    method.getParameterCount() == 1) {
+
+            }
         }
     }
 }
